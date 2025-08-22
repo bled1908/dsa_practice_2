eb783f15d4ffab65fba5b006f73508c1c09896a4
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-22 13:52:08 +0530

    Time: 0 ms (100%), Space: 17.9 MB (48.42%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,20 +1,26 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[-1] * (n + 1) for _ in range(m + 1)]
+        dp = [[0] * (n) for _ in range(m)]
+        dp[0][0] = 0 if obstacleGrid[0][0] == 1 else 1
+        for i in range(1, n):
+            if obstacleGrid[0][i - 1] == 1:
+                break
+            else:
+                dp[0][i] = 1
+        for i in range(1, m):
+            if obstacleGrid[i - 1][0] == 1:
+                break
+            else:
+                dp[i][0] = 1
+        for row in range(1, m):
+            for col in range(1, n):
+                new_row = 0 if obstacleGrid[row][col - 1] else dp[row][col - 1]
+                new_col = 0 if obstacleGrid[row - 1][col] else dp[row - 1][col]
+                dp[row][col] = new_row + new_col
+        return 0 if obstacleGrid[m - 1][n - 1] else dp[m - 1][n - 1]   
 
-        def recursion(row, column):
-            if row < 0 or column < 0:
-                return 0
-            if obstacleGrid[row][column] == 1:
-                return 0
-            if row == 0 and column == 0:
-                return 1
-            if dp[row][column] != -1:
-                return dp[row][column]
-            dp[row][column] = recursion(row - 1, column) + recursion(row, column - 1)
-            return dp[row][column]
-        
-        if obstacleGrid[0][0]:
-            return 0
-        return recursion(m - 1, n - 1)
+        # for debugging
+        for row in dp:
+            print(*row)
+
