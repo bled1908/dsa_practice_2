a59c8fde383b37aec7c0705ed870a75574b79fc1
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-22 09:27:58 +0530

    Time: 0 ms (100%), Space: 17.8 MB (53.8%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,16 @@
 class Solution:
+    def totalAmount(self, n, nums, dp):
+        if n < 0:
+            return 0
+        if dp[n] != -1:
+            return dp[n]
+        rob = nums[n] + self.totalAmount(n - 2, nums, dp)
+        skip = self.totalAmount(n - 1, nums, dp)
+        dp[n] = max(rob, skip)
+        return dp[n] 
+
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0] * (n + 1)
-        if n < 2:
-            return nums[0]
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, n):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
-        return dp[n - 1]
+        dp = [-1] * (n + 1)
+        return self.totalAmount(n - 1, nums, dp)
+        
