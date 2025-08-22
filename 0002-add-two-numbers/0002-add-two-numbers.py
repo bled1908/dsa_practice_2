d0d41b32b60e53d47e3aafa2236a4af6686810be
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-22 10:45:26 +0530

    Time: 0 ms (100%), Space: 18.1 MB (14.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,14 +8,13 @@ class Solution:
         dummyHead = ListNode(0)
         curr = dummyHead
         carry = 0
-        while l1 != None or l2 != None or carry != 0:
+        while l1 or l2 or carry:
             l1val = l1.val if l1 else 0
             l2val = l2.val if l2 else 0
             col_val = l1val + l2val + carry
-            carry = col_val // 10
-            real_val = ListNode(col_val % 10)
-            curr.next = real_val
+            carry = col_val // 10 
+            curr.next = ListNode(col_val % 10)
             curr = curr.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
         return dummyHead.next
