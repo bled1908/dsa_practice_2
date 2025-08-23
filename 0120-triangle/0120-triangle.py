485d0e00ee60e09960ff383bd565a8443ca7a1fe
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-23 12:20:06 +0530

    Time: 5 ms (42.44%), Space: 19.8 MB (16.69%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,16 +1,17 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         m = len(triangle)
-        dp = {}
+        n = len(triangle[m - 1])
+        dp = [[10001] * (n) for _ in range(m)]
 
         def recursion(row, column):
             if row == m - 1:
                 return triangle[row][column]
-            if (row, column) in dp:
-                return dp[(row, column)]
+            if dp[row][column] != 10001:
+                return dp[row][column]
 
-            dp[(row, column)] = triangle[row][column] + min(recursion(row + 1, column), recursion(row + 1, column + 1))
-            return dp[(row, column)]
+            dp[row][column] = triangle[row][column] + min(recursion(row + 1, column), recursion(row + 1, column + 1))
+            return dp[row][column]
 
         return recursion(0, 0)
         # for debugging
