a9f15aba14fa4d3fc143102b3e262c85139eace4
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-23 10:40:50 +0530

    Time: 30 ms (25.02%), Space: 21.5 MB (14.98%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [[-1] * (n + 1) for _ in range(m + 1)]
+
+        def recursion(row, column):
+            if row < 0 or column < 0:
+                return float('inf')
+            if row == 0 and column == 0:
+                return grid[0][0]
+            if dp[row][column] != -1:
+                return dp[row][column]
+            dp[row][column] = grid[row][column]  + min(recursion(row - 1, column), recursion(row, column - 1))
+            return dp[row][column]
+
+        return recursion(m - 1, n - 1)
+            
+        
