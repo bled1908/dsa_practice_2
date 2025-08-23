a20575fd4eeab9481f38c27cf117de346bca809e
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-23 11:07:34 +0530

    Time: 11 ms (91.15%), Space: 20.1 MB (52.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,19 +1,20 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = [[-1] * (n + 1) for _ in range(m + 1)]
-        dp[0][0]
+        dp = [[-1] * (n) for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, n):
+            dp[0][i] = grid[0][i] + dp[0][i - 1]
+        for i in range(1, m):
+            dp[i][0] = grid[i][0] + dp[i - 1][0]
 
-        def recursion(row, column):
-            if row < 0 or column < 0:
-                return float('inf')
-            if row == 0 and column == 0:
-                return grid[0][0]
-            if dp[row][column] != -1:
-                return dp[row][column]
-            dp[row][column] = grid[row][column]  + min(recursion(row - 1, column), recursion(row, column - 1))
-            return dp[row][column]
+        for row in range(1, m):
+            for col in range(1, n):
+                dp[row][col] = grid[row][col] + min(dp[row][col - 1], dp[row - 1][col])
+        return dp[m - 1][n - 1]
 
-        return recursion(m - 1, n - 1)
+        # for debugging
+        for row in dp:
+            print(*row)
             
         
