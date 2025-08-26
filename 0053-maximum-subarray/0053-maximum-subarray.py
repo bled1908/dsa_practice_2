037447471e81966a2a46654270fed2c8e90ab4f4
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-26 11:56:36 +0530

    Time: 93 ms (22.41%), Space: 32 MB (66.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,31 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
-        def maxCrossingSum(nums, left, mid, right):
-            # Left half (max suffix sum)
-            left_sum = float('-inf')
-            total = 0
-            for i in range(mid, left - 1, -1):
-                total += nums[i]
-                left_sum = max(left_sum, total)
-
-            # Right half (max prefix sum)
-            right_sum = float('-inf')
-            total = 0
-            for i in range(mid + 1, right + 1):
-                total += nums[i]
-                right_sum = max(right_sum, total)
-            
-            return left_sum + right_sum
-        
-        def maxSubArrayHelper(nums, left, right):
-            if left == right:
-                return nums[left]
-            mid = (left + right) // 2
-            left_sum = maxSubArrayHelper(nums, left, mid)
-            right_sum = maxSubArrayHelper(nums, mid + 1, right)
-            cross_sum = maxCrossingSum(nums, left, mid, right)
-
-            return max(left_sum, right_sum, cross_sum)
-
-        return maxSubArrayHelper(nums, 0, len(nums) - 1)
+        curr_sum = nums[0]
+        max_sum = nums[0]
+        for i in range(1, len(nums)):
+            curr_sum = max(nums[i], curr_sum + nums[i])
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
