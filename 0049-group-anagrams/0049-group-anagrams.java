af2e37b2291bf185a16979fe143c1954eead29ee
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-27 12:53:23 +0530

    Time: 6 ms (98.67%), Space: 48.4 MB (18.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,33 +1,14 @@
+// import java.util.*;
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        int n = strs.length;
-        boolean[] used = new boolean[n];
+        HashMap<String, List<String>> anagramMap = new HashMap<>();
 
-        for(int i = 0; i < n; i++) {
-            if (used[i]) continue;
-            
-            List<String> group = new ArrayList<>();
-            group.add(strs[i]);
-            used[i] = true;
-
-            for(int j = i + 1; j < n; j++) {
-                if(!used[j] && isAnagram(strs[i], strs[j])) {
-                    group.add(strs[j]);
-                    used[j] = true;
-                }
-            }
-            res.add(group);
+        for(String word: strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            anagramMap.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
         }
-        return res;
-    }
-
-    private boolean isAnagram(String a, String b) {
-        if (a.length() != b.length()) return false;
-        char[] arrA = a.toCharArray();
-        char[] arrB = b.toCharArray();
-        Arrays.sort(arrA);
-        Arrays.sort(arrB);
-        return Arrays.equals(arrA, arrB);
+        return new ArrayList<>(anagramMap.values());
     }
 }
