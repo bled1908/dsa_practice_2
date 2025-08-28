c689f4105a2f622df054dfa8123855057e663b27
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-28 13:47:03 +0530

    Time: 11 ms (72.39%), Space: 17.9 MB (50.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) == sorted(t):
-            return True
-        return False
+        # from collections import Counter
+        # return Counter(s) == Counter(t)
+        # only for english letters
+        if len(s) != len(t):
+            return False
+        
+        count = [0] * 26
+        for ch in s:
+            count[ord(ch) - ord('a')] += 1
+        for ch in t:
+            count[ord(ch) - ord('a')] -= 1
+        
+        for c in count:
+            if c != 0:
+                return False
+        return True
