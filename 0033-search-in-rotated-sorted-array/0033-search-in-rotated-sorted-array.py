09b7899ca9a55dc45080abcdc2ed4dc325b8470c
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-28 16:05:20 +0530

    Time: 0 ms (100%), Space: 18.3 MB (22.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,6 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i, num in enumerate(nums):
-            if num == target:
-                return i
+        n = len(nums)
+
+        # find index of smallest element(pivot)
+        left, right = 0, n - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        pivot = left
+
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            real_mid = (mid + pivot) % n
+            if nums[real_mid] == target:
+                return real_mid
+            if nums[real_mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
         return -1
+        
