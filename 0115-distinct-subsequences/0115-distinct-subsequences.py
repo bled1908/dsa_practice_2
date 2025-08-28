4fcdd19f881970b197b3fb3514b5fa8c29499106
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-08-28 12:14:26 +0530

    Time: 380 ms (74.44%), Space: 41.9 MB (71.62%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,15 +1,19 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n1, n2 = len(s), len(t)
-        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
-        for i in range(n1 + 1):
-            dp[i][0] = 1
+        dp = [[-1] * (n2 + 1) for _ in range(n1 + 1)]
         # match - add the top and top - left (left diagonal) cell
         # mismatch - copy the top cell
-        for row in range(1, n1 + 1):
-            for col in range(1, n2 + 1):
-                if s[row - 1] == t[col - 1]:
-                    dp[row][col] = dp[row - 1][col] + dp[row - 1][col - 1]
-                else:
-                    dp[row][col] = dp[row - 1][col]
-        return dp[n1][n2]
+        def helper(i, j):
+            if dp[i][j] != -1:
+                return dp[i][j]
+            if j < 0:
+                return 1
+            if i < 0:
+                return 0
+            if s[i] == t[j]:
+                dp[i][j] = helper(i - 1, j - 1) + helper(i - 1, j)
+            else:
+                dp[i][j] = helper(i - 1, j)
+            return dp[i][j]
+        return helper(n1 - 1, n2 - 1)
