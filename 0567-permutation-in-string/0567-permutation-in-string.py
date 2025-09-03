d7e1c0e524868a73b4dfe7ca5f851abcb5c0d5b9
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-09-03 22:56:24 +0530

    Time: 3043 ms (7.44%), Space: 18.2 MB (19.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,16 +1,12 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        dict1 = {}
-        for ch in s1:
-            dict1[ch] = dict1.get(ch, 0) + 1
+        s1 = sorted(s1)
         n1, n2 = len(s1), len(s2)
         left = 0
         for right in range(n1 - 1, n2):
             s = s2[left:right + 1]
-            dict2 = {}
-            for ch in s:
-                dict2[ch] = dict2.get(ch, 0) + 1
-            if dict1 == dict2:
+            s = sorted(s)
+            if s == s1:
                 return True
             left += 1
         return False
