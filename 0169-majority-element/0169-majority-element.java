208622d0ac9592e7aaa5fdf778ab006f2f377eaa
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-09-15 10:30:24 +0530

    Time: 4 ms (49.94%), Space: 53.5 MB (14.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,12 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        int low = 0, high = nums.length - 1;
-        int mid = (low + high) / 2;
-        if(nums[low] == nums[mid]) {
-            return nums[low];
-        } else {
-            return nums[mid];
-        }
+        int mid = (0 + nums.length) / 2;
+        return nums[mid];
     }
 }
