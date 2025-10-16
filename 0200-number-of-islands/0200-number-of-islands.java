2edb8eceb88395c2a780196fd4f4e17628743c2a
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-10-16 09:31:09 +0530

    Time: 3 ms (84.59%), Space: 49.7 MB (46.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,29 @@
 class Solution {
+    int R;
+    int C;
+
     public int numIslands(char[][] grid) {
-        int R = grid.length;
-        int C = grid[0].length;
+        if(grid == null || grid.length == 0) return 0;
+        R = grid.length;
+        C = grid[0].length;
         int islandCount = 0;
         for(int r = 0; r < R; r++) {
             for(int c = 0; c < C; c++) {
                 if(grid[r][c] == '1') {
                     islandCount++;
-                    sinkIsland(grid, r, c, R, C);
+                    sinkIsland(grid, r, c);
                 }
             }
         }
         return islandCount;
     }
 
-    private void sinkIsland(char[][] grid, int r, int c, int R, int C) {
+    private void sinkIsland(char[][] grid, int r, int c) {
         if(r < 0 || r >= R || c < 0 || c >= C || grid[r][c] == '0') return;
         grid[r][c] = '0';
-        sinkIsland(grid, r - 1, c, R, C); 
-        sinkIsland(grid, r + 1, c, R, C); 
-        sinkIsland(grid, r, c - 1, R, C); 
-        sinkIsland(grid, r, c + 1, R, C); 
+        sinkIsland(grid, r - 1, c); 
+        sinkIsland(grid, r + 1, c); 
+        sinkIsland(grid, r, c - 1); 
+        sinkIsland(grid, r, c + 1); 
     }
 }
