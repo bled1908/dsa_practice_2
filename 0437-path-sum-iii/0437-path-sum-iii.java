ec6453eb484264634aec5f0e82467ff2c155351b
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-10-25 19:29:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -15,20 +15,20 @@
  */
 class Solution {
     long count = 0;
-
-    private void dfs(TreeNode node, long curr_sum, int targetSum) {
-        if(node == null) return;
-        curr_sum += node.val;
-        if(curr_sum == targetSum) count += 1;
-        dfs(node.left, curr_sum, targetSum);
-        dfs(node.right, curr_sum, targetSum);
+    private void dfs(TreeNode root, int targetSum, int currSum) {
+        if(root == null) {
+            return;
+        }
+        currSum += root.val;
+        if(currSum == targetSum) count += 1;
+        dfs(root.left, targetSum, currSum);
+        dfs(root.right, targetSum, currSum);
     }
-
     public int pathSum(TreeNode root, int targetSum) {
         if(root == null) return 0;
-        dfs(root, 0, targetSum);
-        pathSum(root.left, targetSum);
-        pathSum(root.right, targetSum);
+        dfs(root, targetSum, 0);
+        dfs(root.left, targetSum, 0);
+        dfs(root.right, targetSum, 0);
         return (int) count;
     }
 }
