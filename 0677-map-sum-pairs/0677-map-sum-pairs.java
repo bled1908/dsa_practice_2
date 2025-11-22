a9e12c5b3a69416881de1142cc1fea3ac79adb77
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2025-11-22 22:43:07 +0530

    Time: 9 ms (88.35%), Space: 44 MB (8.83%) - LeetHub

---
+++ 0677-map-sum-pairs/0677-map-sum-pairs.java
@@ -0,0 +1,21 @@
+class MapSum {
+    Map<String, Integer> map;
+
+    public MapSum() {
+        map = new HashMap<>();
+    }
+    
+    public void insert(String key, int val) {
+        map.put(key, val);
+    }
+    
+    public int sum(String prefix) {
+        int sum = 0;
+        for (String key : map.keySet()) {
+            if (key.startsWith(prefix)) {
+                sum += map.get(key);
+            }
+        }
+        return sum;
+    }
+}
