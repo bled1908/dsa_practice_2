3a30534bc7cd9f748ac0f42553c4f7dc019a9e98
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-01-21 10:21:54 +0530

    Time: 3 ms (87.3%), Space: 52.3 MB (50.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,27 @@
 class Solution {
-    int R;
-    int C;
-
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0) return 0;
-        R = grid.length;
-        C = grid[0].length;
-        int islandCount = 0;
-        for(int r = 0; r < R; r++) {
-            for(int c = 0; c < C; c++) {
+        int numOfIslands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
                 if(grid[r][c] == '1') {
-                    islandCount++;
-                    sinkIsland(grid, r, c);
+                    numOfIslands++;
+                    dfs(grid, r, c);
                 }
             }
         }
-        return islandCount;
+        return numOfIslands;
     }
-
-    private void sinkIsland(char[][] grid, int r, int c) {
-        if(r < 0 || r >= R || c < 0 || c >= C || grid[r][c] == '0') return;
+    private void dfs(char[][] grid, int r, int c) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if(r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') return;
         grid[r][c] = '0';
-        sinkIsland(grid, r - 1, c); 
-        sinkIsland(grid, r + 1, c); 
-        sinkIsland(grid, r, c - 1); 
-        sinkIsland(grid, r, c + 1); 
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
     }
 }
