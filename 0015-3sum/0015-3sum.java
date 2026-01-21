8f7453fdb5aa3fd4f21f8ed63becae2ea82b6151
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-01-21 10:32:25 +0530

    Time: 31 ms (79.41%), Space: 59.2 MB (52.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,21 +2,20 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        int n = nums.length;
-        for(int i = 0; i < n - 2; i++) {
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(nums[i] > 0) break;
             if(i > 0 && nums[i] == nums[i - 1]) continue;
-            int target = 0 - nums[i];
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.length - 1;
             while(left < right) {
-                int cur_sum = nums[left] + nums[right];
-                if(cur_sum == target) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     while(left < right && nums[left] == nums[left + 1]) left++;
                     while(left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
-                } else if(cur_sum < target) left++;
+                } else if(sum < 0) left++;
                 else right--;
             }
         }
