e1891a007f65351da3ab562dcee1213944c59261
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-02-11 10:00:18 +0530

    Time: 325 ms (59.19%), Space: 47.1 MB (68.2%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -1,19 +1,19 @@
 class Solution {
     public int longestBalanced(int[] nums) {
         int n = nums.length;
-        int res = 0;
+        int maxLen = 0;
         for(int i = 0; i < n; i++) {
             Set<Integer> seen = new HashSet<>();
-            int oddCount = 0, evenCount = 0;
-            for(int j =  i; j < n; j++) {
+            int evenCount = 0, oddCount = 0;
+            for(int j = i; j < n; j++) {
                 if(seen.add(nums[j])) {
                     if(nums[j] % 2 == 0) evenCount++;
                     else oddCount++;
                 }
                 if(evenCount == oddCount) 
-                    res = Math.max(res, j - i + 1);
+                    maxLen = Math.max(maxLen, j - i + 1);
             }
         }
-        return res;
+        return maxLen;
     }
 }
