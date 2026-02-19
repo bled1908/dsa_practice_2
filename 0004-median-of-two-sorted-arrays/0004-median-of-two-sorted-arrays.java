5fc60b3dbeb85df7ffd7a62bef4ca3f08e8f7fc6
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-02-19 18:54:03 +0530

    Time: 1 ms (99.58%), Space: 49.1 MB (31.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,23 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        int total = n1 + n2;
-        int mid1 = (total - 1) / 2, mid2 = total / 2;
-        int i = 0, j = 0, k = 0;
-        int curr = 0, prev = 0, count = 0;
-        while(count <= mid2) {
-            prev = curr;
-            if(i < n1 && (j >= n2 || nums1[i] <= nums2[j])) {
-                curr = nums1[i++];
-            } else {
-                curr = nums2[j++];
+        if(nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int m = nums1.length, n = nums2.length, low = 0, high = m;
+        while(low <= high) {
+            int partA = low + (high - low) / 2;
+            int partB = (m + n + 1) / 2 - partA;
+
+            int maxLeftA = (partA == 0) ? Integer.MIN_VALUE : nums1[partA - 1];
+            int maxLeftB = (partB == 0) ? Integer.MIN_VALUE : nums2[partB - 1];
+
+            int minRightA = (partA == m) ? Integer.MAX_VALUE : nums1[partA];
+            int minRightB = (partB == n) ? Integer.MAX_VALUE : nums2[partB];
+
+            if(maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if((m+n) % 2 == 0) 
+                    return (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2.0;
+                else 
+                    return Math.max(maxLeftA, maxLeftB);
             }
-            count++;
+            else if(maxLeftA > minRightB) 
+                high = partA - 1;
+            else 
+                low = partA + 1;
         }
-        if((total & 1) == 1) {
-            return curr;
-        } else {
-            return (curr + prev) / 2.0;
-        }
+        throw new IllegalArgumentException("Input arrays are not sorted");
     }
 }
