a634a355db2503149c03008195b4e18c1add9ff9
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-02-19 19:43:39 +0530

    Time: 18 ms (90.53%), Space: 47.4 MB (99.1%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -1,49 +1,41 @@
-import java.util.PriorityQueue;
-
 class Solution {
-    public int trapRainWater(int[][] height) {
-        int n = height.length;
-        int m = height[0].length;
+    public int trapRainWater(int[][] heightMap) {
+        if(heightMap == null || heightMap.length < 3 || heightMap[0].length < 3) return 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        boolean[][] vis = new boolean[n][m];
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        boolean[][] visited = new boolean[m][n];
 
-        // Add first and last column
-        for (int i = 0; i < n; i++) {
-            vis[i][0] = true;
-            vis[i][m - 1] = true;
-            pq.offer(new int[]{height[i][0], i, 0});
-            pq.offer(new int[]{height[i][m - 1], i, m - 1});
-        }
-
-        // Add first and last row
-        for (int i = 0; i < m; i++) {
-            vis[0][i] = true;
-            vis[n - 1][i] = true;
-            pq.offer(new int[]{height[0][i], 0, i});
-            pq.offer(new int[]{height[n - 1][i], n - 1, i});
-        }
-
-        int ans = 0;
-        int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, -1, 0, 1};
-
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int h = curr[0], r = curr[1], c = curr[2];
-
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc]) {
-                    ans += Math.max(0, h - height[nr][nc]);
-                    pq.offer(new int[]{Math.max(h, height[nr][nc]), nr, nc});
-                    vis[nr][nc] = true;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    minHeap.offer(new int[] {i, j, heightMap[i][j]});
+                    visited[i][j] = true;
                 }
             }
         }
 
-        return ans;
+        int waterTrapped = 0;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while(!minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int row = curr[0], col = curr[1], height = curr[2];
+
+            for(int[] dir: directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited[newRow][newCol]) {
+                    if(heightMap[newRow][newCol] < height) {
+                        waterTrapped += height - heightMap[newRow][newCol];
+                    }
+                    minHeap.offer(new int[] {newRow, newCol, Math.max(height, heightMap[newRow][newCol])});
+                    visited[newRow][newCol] = true;
+                }
+            }
+        }
+        return waterTrapped;
     }
 }
