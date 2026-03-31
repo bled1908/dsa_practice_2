c2593fe2bea40e39c9b69cd3a814d465f9613740
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-03-31 15:43:06 +0530

    Time: 3 ms (98.94%), Space: 44.2 MB (84.03%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,47 +1,20 @@
 class Solution {
-  public String reverseWords(String s) {
-    StringBuilder sb = new StringBuilder(s).reverse(); // Reverse the whole string.
-    reverseWords(sb, sb.length());                     // Reverse each word.
-    return cleanSpaces(sb, sb.length());               // Clean up the spaces.
-  }
-
-  private void reverseWords(StringBuilder sb, int n) {
-    int i = 0;
-    int j = 0;
-
-    while (i < n) {
-      while (i < j || i < n && sb.charAt(i) == ' ') // Skip the spaces.
-        ++i;
-      while (j < i || j < n && sb.charAt(j) != ' ') // Skip the spaces.
-        ++j;
-      reverse(sb, i, j - 1); // Reverse the word.
+    public String reverseWords(String s) {
+        if(s == null) return null;
+        StringBuilder sb = new StringBuilder();
+        int i = s.length() - 1;
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            if(i < 0) break;
+            int end = i;
+            while(i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+            if(sb.length() != 0) sb.append(" ");
+            sb.append(s.substring(i + 1, end + 1));
+        }
+        return sb.toString();
     }
-  }
-
-  // Trim leading, trailing, and middle spaces
-  private String cleanSpaces(StringBuilder sb, int n) {
-    int i = 0;
-    int j = 0;
-
-    while (j < n) {
-      while (j < n && sb.charAt(j) == ' ') // Skip the spaces.
-        ++j;
-      while (j < n && sb.charAt(j) != ' ') // Keep non spaces
-        sb.setCharAt(i++, sb.charAt(j++));
-      while (j < n && sb.charAt(j) == ' ') // Skip the spaces.
-        ++j;
-      if (j < n) // Keep only one space.
-        sb.setCharAt(i++, ' ');
-    }
-
-    return sb.substring(0, i).toString();
-  }
-
-  private void reverse(StringBuilder sb, int l, int r) {
-    while (l < r) {
-      final char temp = sb.charAt(l);
-      sb.setCharAt(l++, sb.charAt(r));
-      sb.setCharAt(r--, temp);
-    }
-  }
 }
