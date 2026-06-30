e47c98d122342569680e8eae8efdaf92238cf0fd
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-06-30 23:16:42 +0530

    Time: 13 ms (12.27%), Space: 47.5 MB (8.56%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,15 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1) return s;
-        StringBuilder a = new StringBuilder();
-        for (int i = 0; i < numRows; i++) {
-            for (int j = i; j < s.length(); j += (2 * (numRows - 1))) {
-                a.append(s.charAt(j));
-                if (i > 0 && i < numRows - 1 && j + (2 * (numRows - 1)) - (2 * i) < s.length()) {
-                    a.append(s.charAt(j + (2 * (numRows - 1)) - (2 * i)));
-                }
+        List<List<Character>> mat = new ArrayList<>();
+        for(int r = 0;r<numRows;r++)
+        {
+            mat.add(new ArrayList<>());
+        }
+        int n = s.length();
+        int i = 0;
+        while(i<n)
+        {
+            for(int down = 0;down<numRows && i<n;down++)
+            {
+                mat.get(down).add(s.charAt(i++));
+            }
+            for(int up = numRows-2;up>0  && i<n;up--)
+            {
+                mat.get(up).add(s.charAt(i++));
             }
         }
-        return a.toString();
+        String ans = "";
+        for(i=0;i<numRows;i++)
+        {
+            for(int j=0;j<mat.get(i).size();j++)
+            ans += mat.get(i).get(j);
+        }
+        return ans;
     }
 }
