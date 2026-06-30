5ee8e56ed9ba7ca452f1257114ca51b48180562f
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-06-30 23:14:38 +0530

    Time: 1 ms (100%), Space: 48.8 MB (51.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,30 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-        int m = nums1.length, n = nums2.length, low = 0, high = m;
-        while(low <= high) {
-            int partA = low + (high - low) / 2;
-            int partB = (m + n + 1) / 2 - partA;
-
-            int maxLeftA = (partA == 0) ? Integer.MIN_VALUE : nums1[partA - 1];
-            int maxLeftB = (partB == 0) ? Integer.MIN_VALUE : nums2[partB - 1];
-
-            int minRightA = (partA == m) ? Integer.MAX_VALUE : nums1[partA];
-            int minRightB = (partB == n) ? Integer.MAX_VALUE : nums2[partB];
-
-            if(maxLeftA <= minRightB && maxLeftB <= minRightA) {
-                if((m+n) % 2 == 0) 
-                    return (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2.0;
-                else 
-                    return Math.max(maxLeftA, maxLeftB);
+      int   i=0;
+      int j=0;
+      int k=0;
+      int n=nums1.length;
+      int m=nums2.length;
+      int []arr=new int [n+m];
+        while(i<n||j<m){
+            if(i<n&&j<m)
+            {
+            if(nums1[i]<nums2[j]){
+                arr[k++]=nums1[i++];
             }
-            else if(maxLeftA > minRightB) 
-                high = partA - 1;
-            else 
-                low = partA + 1;
+            else{
+                arr[k++]=nums2[j++];
+            }
+            }
+            else if(i<n){
+                arr[k++]=nums1[i++];
+            }
+            else{
+                arr[k++]=nums2[j++];
+            }
         }
-        throw new IllegalArgumentException("Input arrays are not sorted");
+        float result;
+        int  mid=arr.length/2;
+        if(arr.length%2==0){
+            result=(arr[mid]+arr[mid-1])/2.0f;
+        }
+        else{
+            result=arr[mid];
+        }
+        return result;
     }
 }
