22c8018809baf40215688aa82d6b9a97e922614a
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-01 23:03:44 +0530

    Time: 166 ms (6.38%), Space: 47.6 MB (5.31%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,29 +1,30 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        if(n < 4) return res;
         Arrays.sort(nums);
-        for(int i = 0; i < n - 3; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-            for(int j = i + 1; j < n - 2; j++) {
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
-                long target1 = (long) target - nums[i] - nums[j];
-                int left = j + 1;
-                int right = n - 1;
-                while(left < right) {
-                    long cur_sum = (long) nums[left] + nums[right];
-                    if(cur_sum == target1) {
-                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        while(left < right && nums[left] == nums[left + 1]) left++;
-                        while(left < right && nums[right] == nums[right - 1]) right--;
-                        left++;
-                        right--;
-                    } else if(cur_sum < target1) left++;
-                    else right--;
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int k = j + 1;
+                int l = nums.length - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        s.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
                 }
             }
         }
-        return res;
+        output.addAll(s);
+        return output;
     }
 }
