322f41bfe8bdccdb4b4eba053a11f719df69e7a7
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-02 13:11:54 +0530

    Time: 0 ms (100%), Space: 46.3 MB (74.71%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -1,48 +1,58 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-    ListNode begin;
-    if (head==null || head.next ==null || k==1)
-    	return head;
-    ListNode dummyhead = new ListNode(-1);
-    dummyhead.next = head;
-    begin = dummyhead;
-    int i=0;
-    while (head != null){
-    	i++;
-    	if (i%k == 0){
-    		begin = reverse(begin, head.next);
-    		head = begin.next;
-    	} else {
-    		head = head.next;
-    	}
-    }
-    return dummyhead.next;
-    
-}
 
-public ListNode reverse(ListNode begin, ListNode end){
-	ListNode curr = begin.next;
-	ListNode next, first;
-	ListNode prev = begin;
-	first = curr;
-	while (curr!=end){
-		next = curr.next;
-		curr.next = prev;
-		prev = curr;
-		curr = next;
-	}
-	begin.next = prev;
-	first.next = curr;
-	return first;
-}
+    private ListNode[] reverseList(ListNode head, int k)
+    {
+        ListNode curr = head;
+        ListNode prev = null, next = null;
+
+       for(int i = 0; i < k; i++){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        } 
+
+        return new ListNode[] {prev, curr};
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode ptr = dummy;
+
+        while(ptr != null)
+        {
+           ListNode tracker = ptr;
+
+           for(int i = 0; i < k; i++)
+           {
+            if(tracker == null) break;
+
+            tracker = tracker.next;
+           }
+
+        if (tracker == null) {
+            break;
+            }
+
+           ListNode[] reversedData = reverseList(ptr.next, k);
+
+           ListNode prev = reversedData[0];
+           ListNode curr = reversedData[1];
+
+           ListNode lastNodeOfReversedGroup = ptr.next;
+
+
+           lastNodeOfReversedGroup.next = curr;
+
+           ptr.next = prev;
+
+           ptr = lastNodeOfReversedGroup;
+           
+        }
+
+        return dummy.next;
+    }
 }
