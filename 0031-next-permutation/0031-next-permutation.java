687aae4aae534f84e1d649df2d428521e073217a
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-03 23:55:42 +0530

    Time: 0 ms (100%), Space: 44.5 MB (83.55%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,27 +1,48 @@
 class Solution {
+
     public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while(i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        if(i >= 0) {
-            int j = nums.length - 1;
-            while(nums[j] <= nums[i]) {
-                j--;
+
+        int index = -1;
+
+        // Step 1: Find the first decreasing element from the end
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
             }
-            swap(nums, i, j);
         }
-        reverse(nums, i + 1);
+
+        // Step 2: If no such element exists, reverse the whole array
+        if (index == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+
+        // Step 3: Find the next greater element and swap
+        for (int i = nums.length - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums, i, index);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the remaining part
+        reverse(nums, index + 1, nums.length - 1);
     }
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    } 
-    private void reverse(int[] nums, int start) {
-        int end = nums.length - 1;
-        while(start < end) {
-            swap(nums, start++, end--);
+
+    // Reverse the array from left to right
+    public void reverse(int[] arr, int left, int right) {
+        while (left < right) {
+            swap(arr, left, right);
+            left++;
+            right--;
         }
+    }
+
+    // Swap two elements
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
