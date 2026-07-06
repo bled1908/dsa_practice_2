7fc9dbe4580f89c8ceeb70482a78dd046ab24f0e
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-06 22:22:26 +0530

    Time: 0 ms (100%), Space: 43.7 MB (52.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,31 +1,24 @@
 class Solution {
-    private void transpose(int[][] matrix) {
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=i; j<matrix[0].length; j++) {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-    }
-    private void reverseRows(int[][] matrix) {
-        for(int r=0; r<matrix.length; r++) {
-            int left = 0;
-            int right = matrix.length-1;
 
-            while(left < right) {
-                int temp = matrix[r][left];
-                matrix[r][left] = matrix[r][right];
-                matrix[r][right] = temp;
-
-                left++;
-                right--;
+        for (int i = 0; i < n; i++) {
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int temp = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+                l++;
+                r--;
             }
         }
-
-    }
-    public void rotate(int[][] matrix) {
-        transpose(matrix);
-        reverseRows(matrix);
     }
 }
