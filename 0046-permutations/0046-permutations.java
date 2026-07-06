2633f7dbc9061ee95a43305b1a66faa9c27c3889
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-06 22:21:10 +0530

    Time: 2 ms (36.42%), Space: 46.6 MB (5.82%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,39 +1,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> results  = new ArrayList<>();
-        backtrack(0, nums, results);
-        return results;
-    }
-
-    private void backtrack(int index, int[] nums, List<List<Integer>> results) {
-        // If the index has reached the end of the array, we have a complete permutation
-        if(index == nums.length) {
-            results.add(toList(nums));
-            return;
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 1) {
+            List<Integer> singleList = new ArrayList<>();
+            singleList.add(nums[0]);
+            res.add(singleList);
+            return res;
         }
 
-        // Recursive step: Iterate from the current index to the end
-        for (int i = index; i < nums.length; i++) {
-            // Choose: Swap the current element (nums[i]) with the element at the index position
-            swap(nums, index, i);
-
-            // Explore: Recurse to fill the next position
-            backtrack(index + 1, nums, results);
-
-            // Un-choose(Backtrack): Swap the elements back to restore the original array state for teh next iteration of the loop
-            swap(nums, index, i);
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int[] remainingNums = new int[nums.length - 1];
+            int index = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i) {
+                    remainingNums[index] = nums[j];
+                    index++;
+                }
+            }
+            
+            List<List<Integer>> perms = permute(remainingNums);
+            for (List<Integer> p : perms) {
+                p.add(n);
+            }
+            
+            res.addAll(perms);
         }
-    }
-
-    private List<Integer> toList(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for (int num: nums) list.add(num);
-        return list;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        
+        return res;        
     }
 }
