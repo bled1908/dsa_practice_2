0ac11fc4fb996761ba0c7e12a32782f0f65d8bac
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-06 22:19:27 +0530

    Time: 2 ms (99.84%), Space: 43.7 MB (52.37%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,23 +1,34 @@
-public class Solution {
+class Solution {
     public String multiply(String num1, String num2) {
-        int n1 = num1.length(), n2 = num2.length();
-        int[] products = new int[n1 + n2];
-        for (int i = n1 - 1; i >= 0; i--) {
-            for (int j = n2 - 1; j >= 0; j--) {
-                int d1 = num1.charAt(i) - '0';
+        int m = num1.length();
+        int n = num2.length();
+        int[] res = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+
+            for (int j = n - 1; j >= 0; j--) {
                 int d2 = num2.charAt(j) - '0';
-                products[i + j + 1] += d1 * d2;
+                int mul = d1 * d2;
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = mul + res[p2];
+
+                res[p2] = sum % 10;
+                res[p1] += sum / 10;
             }
         }
-        int carry = 0;
-        for (int i = products.length - 1; i >= 0; i--) {
-            int tmp = (products[i] + carry) % 10;
-            carry = (products[i] + carry) / 10;
-            products[i] = tmp;
-        }
+
         StringBuilder sb = new StringBuilder();
-        for (int num : products) sb.append(num);
-        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
+
+        for (int num : res) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
