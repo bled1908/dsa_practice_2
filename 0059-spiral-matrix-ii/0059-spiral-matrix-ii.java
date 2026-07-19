407ede04de8a4cc2d5fa40eed99e70737f844d7d
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-19 18:30:00 +0530

    Time: 0 ms (100%), Space: 42.8 MB (80.22%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,27 +1,44 @@
-class Solution {
-    public static int[][] generateMatrix(int n) {
-	int[][] ret = new int[n][n];
-	int left = 0,top = 0;
-	int right = n -1,down = n - 1;
-	int count = 1;
-	while (left <= right) {
-		for (int j = left; j <= right; j ++) {
-			ret[top][j] = count++;
-		}
-		top ++;
-		for (int i = top; i <= down; i ++) {
-			ret[i][right] = count ++;
-		}
-		right --;
-		for (int j = right; j >= left; j --) {
-			ret[down][j] = count ++;
-		}
-		down --;
-		for (int i = down; i >= top; i --) {
-			ret[i][left] = count ++;
-		}
-		left ++;
-	}
-	return ret;
-}
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        // Declaration
+        int[][] matrix = new int[n][n];
+        
+        // Edge Case
+        if (n == 0) {
+            return matrix;
+        }
+        
+        // Normal Case
+        int rowStart = 0;
+        int rowEnd = n-1;
+        int colStart = 0;
+        int colEnd = n-1;
+        int num = 1; //change
+        
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i ++) {
+                matrix[rowStart][i] = num ++; //change
+            }
+            rowStart ++;
+            
+            for (int i = rowStart; i <= rowEnd; i ++) {
+                matrix[i][colEnd] = num ++; //change
+            }
+            colEnd --;
+            
+            for (int i = colEnd; i >= colStart; i --) {
+                if (rowStart <= rowEnd)
+                    matrix[rowEnd][i] = num ++; //change
+            }
+            rowEnd --;
+            
+            for (int i = rowEnd; i >= rowStart; i --) {
+                if (colStart <= colEnd)
+                    matrix[i][colStart] = num ++; //change
+            }
+            colStart ++;
+        }
+        
+        return matrix;
+    }
 }
