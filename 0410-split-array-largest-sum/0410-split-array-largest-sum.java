d212edff83ca4407833391857dae1b89bc383f81
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-22 11:20:32 +0530

    Time: 0 ms (100%), Space: 43.3 MB (16.78%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,43 +1,35 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
+        if(k > nums.length) return -1;
         int low = 0, high = 0;
-
-        for (int n : nums) {
-            low = Math.max(low, n); 
-            high += n;              
+        for(int num: nums) {
+            low = Math.max(low, num);
+            high += num;
         }
-
-        int ans = high;
-
-        while (low <= high) {
+        int ans = -1;
+        while(low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (canSplit(nums, k, mid)) {
-                ans = mid;          
-                high = mid - 1;     
+            if(canSplit(nums, k, mid)) {
+                ans = mid;
+                high = mid - 1;
             } else {
-                low = mid + 1;      
+                low = mid + 1;
             }
         }
-
         return ans;
     }
-
-    private boolean canSplit(int[] nums, int k, int maxSumAllowed) {
-        int subarrays = 1;
-        int currentSum = 0;
-
-        for (int n : nums) {
-            if (currentSum + n > maxSumAllowed) {
-                subarrays++;        
-                currentSum = n;
-
-                if (subarrays > k) return false;
+    private boolean canSplit(int[] nums, int k, int mid) {
+        int n = 1;
+        int curr = 0;
+        for(int num: nums) {
+            if(curr + num <= mid) {
+                curr += num;
             } else {
-                currentSum += n;
+                n++;
+                curr = num;
+                if(n > k) return false;
             }
         }
-
         return true;
     }
 }
