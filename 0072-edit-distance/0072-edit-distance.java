67b100e03f003447c038be29b9ff205e9876c89a
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-22 07:27:57 +0530

    Time: 5 ms (67.7%), Space: 47.3 MB (18.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,27 @@
-
-class Solution {
+public class Solution {
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n + 1][m + 1];
-
-        for (int i = 0; i <= n; i++) dp[i][0] = i;
-        for (int j = 0; j <= m; j++) dp[0][j] = j;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + Math.min(
-                        dp[i - 1][j - 1],      // replace
-                        Math.min(
-                            dp[i - 1][j],      // delete
-                            dp[i][j - 1]       // insert
-                        )
-                    );
+        int m = word1.length();
+        int n = word2.length();
+        
+        int[][] cost = new int[m + 1][n + 1];
+        for(int i = 0; i <= m; i++)
+            cost[i][0] = i;
+        for(int i = 1; i <= n; i++)
+            cost[0][i] = i;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(word1.charAt(i) == word2.charAt(j))
+                    cost[i + 1][j + 1] = cost[i][j];
+                else {
+                    int a = cost[i][j];
+                    int b = cost[i][j + 1];
+                    int c = cost[i + 1][j];
+                    cost[i + 1][j + 1] = a < b ? (a < c ? a : c) : (b < c ? b : c);
+                    cost[i + 1][j + 1]++;
                 }
             }
         }
-
-        return dp[n][m];
+        return cost[m][n];
     }
 }
