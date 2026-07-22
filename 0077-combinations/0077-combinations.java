6ccf56104445d82684fd2b5d814b4dc82bf9a319
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-22 07:29:40 +0530

    Time: 16 ms (88.48%), Space: 98.2 MB (27.44%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,28 +1,20 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtrack(res, new ArrayList<>(), n, k, 1);
-        return res;
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(ans,new ArrayList<>(),n,k,1);
+        return ans;
     }
+    public void helper(List<List<Integer>> ans,List<Integer> temp,int n,int k,int s){
+        if(temp.size()==k){
+           ans.add(new ArrayList<>(temp));
+           return;
+        } 
+        if(s>n) return;
 
-    private void backtrack(List<List<Integer>> res, List<Integer> curCom, int n, int k, int start) {
-        // Base Case: We have a complete combination of size k
-        if(curCom.size() == k) {
-            res.add(new ArrayList<>(curCom));
-            return;
-        }
-
-        // Recursive step: We explore all valid choices
-        // we only choose numbers from 'start' to 'n'
-        for(int i = start; i <= n; i++) {
-            // Choose: Add the number 'i'
-            curCom.add(i);
-            
-            // Explore: Recurse to find the rest of teh combination, the next call must start from i + 1, to avoid duplicates.
-            backtrack(res, curCom, n, k , i + 1);
-
-            // Unchoose (Backtrack): Remove 'i' to explore other paths
-            curCom.remove(curCom.size() - 1);
+        for(int i=s;i<=n;i++){
+            temp.add(i);
+            helper(ans,temp,n,k,i+1);
+            temp.remove(temp.size()-1);
         }
     }
 }
