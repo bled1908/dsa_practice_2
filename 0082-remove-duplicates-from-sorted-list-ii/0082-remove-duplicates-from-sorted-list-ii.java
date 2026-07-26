4f326d75e6398875e230b0a74c7f9e6c556d0e56
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-26 14:50:50 +0530

    Time: 0 ms (100%), Space: 45.1 MB (38%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -1,37 +1,86 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null||head.next==null)
-        return head;
-        ListNode prev = new ListNode(-1);
-        ListNode dummy = prev;
-        dummy.next=head;
-        ListNode curr = head;
-        while(curr!=null&&curr.next!=null)
-        {
-            if(curr.val==curr.next.val)
-            {
-                while(curr.next!=null&&curr.val==curr.next.val)
-                {
-                    curr=curr.next;
+        if (head == null) return null;
+
+        ListNode start = head;
+        ListNode mover = head;
+        ListNode hold = head;  
+
+        while (mover.next != null) {
+            if (mover.next != null && mover.val == mover.next.val)mover = mover.next;  
+            else {
+                if (head.val == head.next.val) {
+                    mover = mover.next;
+                    head = mover;
+                    start = head;
+                    hold = head;
+                } 
+                else {
+                    if (start != mover) {
+                        mover = mover.next;
+                        hold.next = mover;
+                        start = mover;
+                    } 
+                    else {
+                        mover = mover.next;
+                        hold = start;
+                        start.next = mover;
+                        start = mover;
+                    }
                 }
-                dummy.next=curr.next;
             }
-            else
-            {
-                dummy=dummy.next;
-            }
-            curr=curr.next;
         }
-        return prev.next;
+        if(head.next==null)return head;  
+        if(head==start)return null;
+        if(hold.next.next!=null)hold.next=null;
+        return head;
     }
 }
+
+
+
+
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if (head == null) return null;
+
+//         ListNode start = head;
+//         ListNode mover = head;
+//         ListNode hold = head;  
+
+//         while (mover.next != null) {
+//             if (mover.next != null && mover.val == mover.next.val)mover = mover.next;  
+//             else {
+//                 if (head.val == head.next.val) {
+//                     mover = mover.next;
+//                     head = mover;
+//                     start = head;
+//                     hold = head;
+//                 } 
+//                 else {
+//                     if (start != mover) {
+//                         mover = mover.next;
+//                         hold.next = mover;
+//                         start = mover;
+//                     } 
+//                     else {
+//                         mover = mover.next;
+//                         hold = start;
+//                         start.next = mover;
+//                         start = mover;
+//                     }
+//                 }
+//             }
+//         }
+
+//         mover = head;
+//         start=head;
+
+//         while(start.next!=null)start=start.next;
+//         if(head.next==null)return head;         //For[2]                       _____->this will be removed up.
+//         if(mover.val==start.val)return null;   //For cases like --> [1,1,1] , [1,1,1,2,2,2] ,[2,2]
+//         while(mover.next.val!=start.val)mover=mover.next;
+//         if(mover.next!=start)mover.next=null;
+//         return head;
+//     }
+// }
