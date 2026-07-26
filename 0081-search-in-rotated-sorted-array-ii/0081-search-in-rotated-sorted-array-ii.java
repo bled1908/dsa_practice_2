fa42855a5dba1b8d1cecebe3e7b2df358fc6d8c2
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-26 14:50:34 +0530

    Time: 0 ms (100%), Space: 45.1 MB (47.23%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,24 +1,33 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] == target) return true;
+  static boolean search(int[]arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
 
-            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
-                l++; r--;
-            } 
-            else if (nums[l] <= nums[mid]) {
-                 // left sorted
-                if (nums[l] <= target && target < nums[mid]) r = mid - 1;
-                else l = mid + 1;
-            } 
-            else { 
-                // right sorted
-                if (nums[mid] < target && target <= nums[r]) l = mid + 1;
-                else r = mid - 1;
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return true;
+            }
+
+              //  To handle duplicates
+
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                start ++;
+                end --;
+                continue;
+            }
+            if(arr[mid] >= arr[start]){
+                if(arr[mid] > target && arr[start] <= target){
+                    end = mid-1;
+                }else start = mid+1;
+            } else{
+                if(arr[mid] < target && arr[end] >= target){
+                    start = mid+1;
+                }else end = mid-1;
             }
         }
         return false;
+        }
     }
-}
+
+ 
