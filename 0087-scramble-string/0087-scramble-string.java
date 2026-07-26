142215f96bab98298c5b6ddbd87ce00d90d7c432
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-26 14:52:43 +0530

    Time: 3 ms (98.83%), Space: 45.6 MB (92.39%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.java
+++ 0087-scramble-string/0087-scramble-string.java
@@ -1,46 +1,41 @@
 class Solution {
-    Map<String, Boolean> mp = new HashMap<>();
-    
-    public boolean helper(String a, String b)
-    {
-        if(a.equals(b) == true) return true;
-        
-        if(a.length() <= 1) return false;
-        
-        int n = a.length();
-        boolean check = false;
-        
-        String key = a;
-        
-        key += ' ' + b;
-        
-        if(mp.containsKey(key))
-             return mp.get(key);
-        
-        for(int i = 1; i < n; i++)
-        {
-            boolean swap = helper(a.substring(0, i), b.substring(n - i)) && helper(a.substring(i), b.substring(0, n - i));
-            boolean unswap = helper(a.substring(0,i), b.substring(0,i)) && helper(a.substring(i), b.substring(i));
-            
-            if(swap || unswap){
-                check = true;
-                break;
+    // to store previously computed substrings
+    Map<String, Boolean> map = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        // check if the two strings are equal
+        if (s1.equals(s2)) {
+            return true;
+        }
+        // initialize frequency arrays for s1, s2, and current substring
+        int[] a = new int[26], b = new int[26], c = new int[26];
+        // check if the current substring has already been computed
+        if (map.containsKey(s1 + s2)) {
+            return map.get(s1 + s2);
+        }
+        // check all possible splits of the two strings
+        for (int i = 1; i <= n - 1; i++) {
+            int j = n - i;
+            // update frequency arrays for s1, s2, and current substring
+            a[s1.charAt(i - 1) - 'a']++;
+            b[s2.charAt(i - 1) - 'a']++;
+            c[s2.charAt(j) - 'a']++;
+            // check if the current substring has the same characters
+            if (Arrays.equals(a, b) && isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i))) {
+                // if the substrings are scrambled versions of each other, return true
+                map.put(s1 + s2, true);
+                return true;
+            }
+            // check if the current substring and its complement have the same characters
+            if (Arrays.equals(a, c) && isScramble(s1.substring(0, i), s2.substring(j)) && isScramble(s1.substring(i), s2.substring(0, j))) {
+                // if the substrings are scrambled versions of each other, return true
+                map.put(s1 + s2, true);
+                return true;
             }
         }
-        
-        mp.put(key, check);
-        
-        return check;
-    }
-    
-    public boolean isScramble(String a, String b)
-    {
-        if(a.equals(b) == true) return true;
-        
-        if(a.length() != b.length()) return false;
-        
-        if(a.isEmpty() == true) return true;
-        
-        return helper(a, b);
+        // if none of the splits result in scrambled versions, return false
+        map.put(s1 + s2, false);
+        return false;
     }
 }
