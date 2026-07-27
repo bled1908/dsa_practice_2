2a64efee63ee97a07b366cdf2b11debf903c7588
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-27 08:40:42 +0530

    Time: 3 ms (74.45%), Space: 43.1 MB (69.36%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,18 +1,22 @@
-class Solution {
+public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-    char[] c1 = s1.toCharArray(), c2 = s2.toCharArray(), c3 = s3.toCharArray();
-	int m = s1.length(), n = s2.length();
-	if(m + n != s3.length()) return false;
-	return dfs(c1, c2, c3, 0, 0, 0, new boolean[m + 1][n + 1]);
-}
+        int m = s1.length(), n = s2.length(), l = s3.length();
+        if (m + n != l) return false;
 
-public boolean dfs(char[] c1, char[] c2, char[] c3, int i, int j, int k, boolean[][] invalid) {
-	if(invalid[i][j]) return false;
-	if(k == c3.length) return true;
-	boolean valid = 
-	    i < c1.length && c1[i] == c3[k] && dfs(c1, c2, c3, i + 1, j, k + 1, invalid) || 
-        j < c2.length && c2[j] == c3[k] && dfs(c1, c2, c3, i, j + 1, k + 1, invalid);
-	if(!valid) invalid[i][j] = true;
-    return valid;
-}
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int j = 1; j <= n; ++j) {
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+            for (int j = 1; j <= n; ++j) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        
+        return dp[n];
+    }
 }
