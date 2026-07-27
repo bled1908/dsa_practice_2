ace5fc31ad9e50254725fd9cdb8f57746717b6bf
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-27 08:37:58 +0530

    Time: 7 ms (19.95%), Space: 50.6 MB (8.32%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,26 +1,24 @@
 class Solution {
-    public void helper(String str, int start,int pCount ,String part ,List<String> result){
-        if(start == str.length() && pCount == 4){
-            result.add(part);
+    List<String> ans = new ArrayList<>();
+    String str;
+    public List<String> restoreIpAddresses(String s) {
+        str = s;
+        magical("", 0, 0);
+        return ans;
+    } 
+
+    void magical( String path, int index, int dots) {
+        if (dots > 4) return;
+        if (dots == 4 && index >= str.length()) {
+            ans.add(path.substring(0,path.length()-1));
             return;
         }
-        if(pCount == 4 || start == str.length()) return;
-
-        for(int i = start + 1; i<= (start + 3) && i <= str.length();i++){
-            String subStr =str.substring(start,i);
-            if(subStr.charAt(0) == '0' && subStr.length() > 1 ) break;
-
-            if(Integer.parseInt(subStr) > 255) break;
-            String newPart = (part == "")? subStr : (part + "." + subStr);
-            helper(str,i,pCount + 1, newPart,result);
+        for (int length = 1; length <= 3 && index + length <= str.length(); length++) {
+            String num = str.substring(index, index + length);
+            if (num.charAt(0) == '0' && length != 1) break;
+            else if (Integer.parseInt(num) <= 255) {
+                magical( path + str.substring(index, index + length) + ".", index + length,dots + 1);
+            }
         }
-    }
-
-    public List<String> restoreIpAddresses(String s) {
-        List<String> result = new ArrayList<>();
-        int n = s.length();
-        if(n > 12 || n < 4) return result;
-        helper(s,0,0,"",result);
-        return result;
     }
 }
