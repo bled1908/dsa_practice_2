1990a9e8c73decb5a2155ff87fc4ce1179a3fb23
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-07-28 12:40:26 +0530

    Time: 1 ms (80.1%), Space: 43.9 MB (9.76%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -13,28 +13,56 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(root == null) return result;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        boolean leftToRight = true;
-        while(!q.isEmpty()) {
-            List<Integer> level = new ArrayList<>();
-            int size = q.size();
-            for(int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                level.add(node.val);
-                if(node.left != null) q.add(node.left);
-                if(node.right != null) q.add(node.right);
+
+     Queue<TreeNode> queue = new LinkedList<>();
+
+    List<List<Integer>> list = new ArrayList<>();
+
+    if(root==null) return new ArrayList<>();
+
+    queue.offer(root);
+
+boolean turn = false;
+    while(!queue.isEmpty()){
+
+List<Integer> ans = new ArrayList<>();
+       
+int s = queue.size();
+        for(int i =0; i < s; i++) {
+            TreeNode node = queue.poll();
+            ans.add(node.val);
+            if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+                
+                
+                
             }
-            if (!leftToRight) {
-                Collections.reverse(level);
-            }
-            result.add(level);
-            leftToRight = !leftToRight;
-        }
-        return result;
+       
+ 
+if(turn) Collections.reverse(ans);
+   turn = !turn;       
+     
+        list.add(new ArrayList<>(ans));
+
     }
+
+
+    return list;}
 }
