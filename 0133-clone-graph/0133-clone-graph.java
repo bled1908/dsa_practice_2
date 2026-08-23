daff8f708e29e1f78aea41a00ccad1bfcad1a60a
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-08-23 17:20:18 +0530

    Time: 24 ms (94.47%), Space: 43.9 MB (97.45%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,43 +1,33 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
-    public Node cloneGraph(Node node) {
-        if(node == null) return null;
-        Map<Node, Node> oldToNew = new HashMap<>();
-        Queue<Node> queue = new LinkedList<>();
-        Node clonedStartNode = new Node(node.val);
-        oldToNew.put(node, clonedStartNode);
-        queue.offer(node);
-        while(!queue.isEmpty()) {
-            Node originalNode = queue.poll();
-            Node clonedNode = oldToNew.get(originalNode);
-            for(Node originalNeighbor: originalNode.neighbors) {
-                if(!oldToNew.containsKey(originalNeighbor)) {
-                    Node clonedNeighbor = new Node(originalNeighbor.val);
-                    oldToNew.put(originalNeighbor, clonedNeighbor);
-                    queue.offer(originalNeighbor);
-                }
-                clonedNode.neighbors.add(oldToNew.get(originalNeighbor));
+    Map<Node, Node> map;
+
+    public void dfs(Node node, Node cloneNode, Map<Node, Node> map) {
+        for (Node neighbor : node.neighbors) {
+
+            // If neighbor is not cloned yet
+            if (!map.containsKey(neighbor)) {
+                Node cloneNeighbor = new Node(neighbor.val);
+                map.put(neighbor, cloneNeighbor);
+
+                cloneNode.neighbors.add(cloneNeighbor);
+                dfs(neighbor, cloneNeighbor, map);
+            } 
+            // If already cloned, reuse it
+            else {
+                cloneNode.neighbors.add(map.get(neighbor));
             }
         }
-        return clonedStartNode;
+    }
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        map = new HashMap<>();
+
+        Node cloneNode = new Node(node.val);
+        map.put(node, cloneNode);
+
+        dfs(node, cloneNode, map);
+        return cloneNode;
     }
 }
