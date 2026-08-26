7ee205a5ea8d70b294e52977892577feeb4bb52b
Subhadeep Roy <136990707+bled1908@users.noreply.github.com>
2026-08-27 01:08:16 +0530

    Time: 3 ms (100%), Space: 103.3 MB (75.72%) - LeetHub

---
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        int n = edges.length;
+        long[] scores = new long[n];
+        for(int i = 0; i < n; i++) {
+            scores[edges[i]] += i;
+        }
+        long maxScore = -1;
+        int bestNode = -1;
+        for(int i = 0; i < n; i++) {
+            if(scores[i] > maxScore) {
+                maxScore = scores[i];
+                bestNode = i;
+            }
+        }
+        return bestNode;
+    }
+}
